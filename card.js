c05afc632b7a47159b493aee49b79ee3002854ce
Antonio D'Angelo <antonio.dangelo@wtbss.com>
2023-12-09 23:14:46 +0100

    integrated wordpress latest 5 posts

--- card.js
+++ card.js
@@ -11,11 +11,9 @@ const fs = require("fs");
 const request = require("request");
 const path = require("path");
 const ora = require("ora");
-const cliSpinners = require("cli-spinners");
 
 clear();
 
-//! importing User Data from data.json
 const res = fs.readFileSync(path.resolve(__dirname, "data.json"));
 const user_data = JSON.parse(res);
 const {
@@ -32,95 +30,146 @@ const {
 
 const prompt = inquirer.createPromptModule();
 
-const questions = [
-    {
-        type: "list",
-        name: "action",
-        message: "What would you like to do?",
-        choices: [
-            //// Send an email
-            {
-                name: `Send me an ${chalk.green.bold("email")}?`,
-                value: () => {
-                    open(`mailto:${user_email}`);
-                    console.log("\nDone, see you soon at inbox.\n");
+async function fetchArticles(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const articles = JSON.parse(body);
+                resolve(articles);
+            } else {
+                reject(error || "Error fetching articles.");
+            }
+        });
+    });
+}
+
+async function showLatestArticles() {
+    const questions = [
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: `Send me an ${chalk.green.bold("email")}?`,
+                    value: () => {
+                        open(`mailto:${user_email}`);
+                        console.log("\nDone, see you soon at inbox.\n");
+                    },
                 },
-            },
-            //// Download Resume
-            {
-                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
-                value: () => {
-                    // cliSpinners.dots;
-                    const loader = ora({
-                        text: " Downloading Resume",
-                        spinner: cliSpinners.material,
-                    }).start();
-                    let pipe = request(`${resume_url}`).pipe(
-                        fs.createWriteStream(`./${npx_card_handle}-resume.pdf`)
-                    );
-                    pipe.on("finish", function () {
-                        let downloadPath = path.join(
-                            process.cwd(),
-                            `${npx_card_handle}-resume.pdf`
+                {
+                    name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+                    value: () => {
+                        const loader = ora({
+                            text: " Downloading Resume",
+                        }).start();
+                        let pipe = request(`${resume_url}`).pipe(
+                            fs.createWriteStream(`./${npx_card_handle}-resume.pdf`)
                         );
-                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
-                        open(downloadPath);
-                        loader.stop();
-                    });
+                        pipe.on("finish", function () {
+                            let downloadPath = path.join(
+                                process.cwd(),
+                                `${npx_card_handle}-resume.pdf`
+                            );
+                            console.log(`\nResume Downloaded at ${downloadPath} \n`);
+                            open(downloadPath);
+                            loader.stop();
+                        });
+                    },
                 },
-            },
-            //// Quit
-            {
-                name: "Just quit.",
-                value: () => {
-                    console.log("See you !\n");
+                {
+                    name: "See my latest articles?",
+                    value: async () => {
+                        const articlesLoader = ora({
+                            text: "Fetching the latest articles",
+                        }).start();
+
+                        const articlesUrl =
+                            "https://blindlystupid.com/wp-json/wp/v2/posts?per_page=5&_embed";
+
+                        try {
+                            const articles = await fetchArticles(articlesUrl);
+                            articlesLoader.stop();
+
+                            const articleQuestion = {
+                                name: "selectedArticle",
+                                type: "list",
+                                message: "Choose an article to open in the browser:",
+                                choices: articles.map((article) => ({
+                                    name: article.title.rendered,
+                                    value: article.link,
+                                })),
+                            };
+
+                            const articleAnswer = await prompt(articleQuestion);
+
+                            // Open the selected article in the default browser
+                            if (articleAnswer.selectedArticle) {
+                                open(articleAnswer.selectedArticle);
+                            }
+                        } catch (error) {
+                            articlesLoader.stop();
+                            console.error(chalk.red("Error fetching or displaying articles:", error));
+                        }
+                    },
                 },
-            },
-        ],
-    },
-];
+                {
+                    name: "Just quit.",
+                    value: () => {
+                        console.log("See you!\n");
+                    },
+                },
+            ],
+        },
+    ];
 
-const data = {
-    name: chalk.bold.green(`                  ${user_name}`),
-    // work: `${chalk.white("Software Engineer at")} ${chalk.hex("#2b82b2").bold("ClearTax")}`,
-    work: `${chalk.white(`${job_title}`)}`,
-    github: chalk.gray("https://github.com/") + chalk.green(`${github_username}`),
-    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue(`${linkedin_username}`),
-    web: chalk.cyan(`${personal_site}`),
-    npx: chalk.red("npx") + " " + chalk.white(`${npx_card_handle}`),
+    const data = {
+        name: chalk.bold.green(`                  ${user_name}`),
+        work: `${chalk.white(`${job_title}`)}`,
+        github: chalk.gray("https://github.com/") + chalk.green(`${github_username}`),
+        linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue(`${linkedin_username}`),
+        web: chalk.cyan(`${personal_site}`),
+        npx: chalk.red("npx") + " " + chalk.white(`${npx_card_handle}`),
+        labelWork: chalk.white.bold("       Work:"),
+        labelGitHub: chalk.white.bold("     GitHub:"),
+        labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+        labelWeb: chalk.white.bold("        Web:"),
+        labelCard: chalk.white.bold("       Card:"),
+    };
 
-    labelWork: chalk.white.bold("       Work:"),
-    labelTwitter: chalk.white.bold("    Twitter:"),
-    labelGitHub: chalk.white.bold("     GitHub:"),
-    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
-    labelWeb: chalk.white.bold("        Web:"),
-    labelCard: chalk.white.bold("       Card:"),
-};
+    const me = boxen(
+        [
+            `${data.name}`,
+            ``,
+            `${data.labelWork}  ${data.work}`,
+            ``,
+            `${data.labelGitHub}  ${data.github}`,
+            `${data.labelLinkedIn}  ${data.linkedin}`,
+            `${data.labelWeb}  ${data.web}`,
+            ``,
+            `${data.labelCard}  ${data.npx}`,
+            ``,
+            `${chalk.italic("I am currently looking for new opportunities,")}`,
+            `${chalk.italic("my inbox is always open. Whether you have a")}`,
+            `${chalk.italic("question or just want to say hi, I will try ")}`,
+            `${chalk.italic("my best to get back to you!")}`,
+        ].join("\n"),
+        {
+            margin: 1,
+            float: "center",
+            padding: 1,
+            borderStyle: "single",
+            borderColor: "green",
+        }
+    );
 
-const me = boxen(
-    [
-        `${data.name}`,
-        ``,
-        `${data.labelWork}  ${data.work}`,
-        ``,
-        `${data.labelGitHub}  ${data.github}`,
-        `${data.labelLinkedIn}  ${data.linkedin}`,
-        `${data.labelWeb}  ${data.web}`,
-        ``,
-        `${data.labelCard}  ${data.npx}`,
-        ``,
-        `${chalk.italic("I am currently looking for new opportunities,")}`,
-        `${chalk.italic("my inbox is always open. Whether you have a")}`,
-        `${chalk.italic("question or just want to say hi, I will try ")}`,
-        `${chalk.italic("my best to get back to you!")}`,
-    ].join("\n"),
-    {
-        margin: 1,
-        float: "center",
-        padding: 1,
-        borderStyle: "single",
-        borderColor: "green",
+    console.log(me);
+
+    const answers = await prompt(questions);
+
+    if (answers.action) {
+        answers.action();
     }
-);
+}
 
-prompt(questions).then(answer => answer.action());
+showLatestArticles();
